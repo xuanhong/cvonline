45d23819de7e8c3a2a2ad082b0fa9136a732bb64
xuanhong <hong.taxuan@gmail.com>
2013-03-14 09:57:39 +0700

    the first

---
+++ cvonline/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvonline::Application.config.secret_token = 'c6fd795228f299114cf1c83920b8a102de428478f4318b1000baa23105f3ed44fb52620a3e04e796d7b5c3df8f5696b8fa2b927ace5fc6fd1695b2bb413ba3b1'
